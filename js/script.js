1a8d4036f23fd42c3825cc3429ce36d703b63c52
Martin Sax <h.martin.sax@gmail.com>
2024-03-26 18:55:58 +0100

    code update

--- js/script.js
+++ js/script.js
@@ -24,16 +24,16 @@ document.addEventListener("DOMContentLoaded", function () {
         productCard.setAttribute("data-description", `${product.description}`);
         productCard.innerHTML = `
                 <div class="card h-100">
-                <div data-bs-toggle="offcanvas" data-bs-target="#offcanvas-product" aria-controls="offcanvas-product">
-                    <img class="card-img-top" src="${product.image}" alt="${product.title}">
-                    <div class="card-body d-flex flex-column justify-content-between">
-                            <h5 class="card-title text-truncate">${product.title}</h5>
-                        </div>
-                        <div class="d-flex flex-column">
-                        <p class="card-text">$${product.price}</p>
-                        <button class="btn btn-primary btn-order">Beställ</button>
-                        </div>
+                  <div data-bs-toggle="offcanvas" data-bs-target="#offcanvas-product" aria-controls="offcanvas-product">
+                    <img class="card-img-top" src="${product.image}" alt="${product.title}">  
+                    <div class="card-body">
+                        <h5 class="card-title text-truncate">${product.title}</h5>
                     </div>
+                  </div>
+                  <div class="d-flex flex-column card-body product-button">
+                    <p class="card-text">$${product.price}</p>
+                    <button class="btn btn-primary btn-order">Beställ</button>
+                  </div>
                 </div>
             `;
         productList.appendChild(productCard);
@@ -77,4 +77,5 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+//dynamiskt år i footer
 document.getElementById("cRyear").innerHTML = new Date().getFullYear();
